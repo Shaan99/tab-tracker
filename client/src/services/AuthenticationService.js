cab35f3d9a4fff536f8f088bc53e4c130740fefd
Shaan Anshu <ashaan677@gmail.com>
2018-10-21 13:15:44 +0530

    made changes in formatting according to eslint

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,13 +1,7 @@
 import Api from '@/services/Api'
 
 export default {
-    register (credentials) {
-        return Api().post('register', credentials)
-    }
+  register (credentials) {
+    return Api().post('register', credentials)
+  }
 }
-
-
-// AunthenticationService.register({
-//     email: 'Shaan.anshu@gmail.com',
-//     password: '123456'
-// })
